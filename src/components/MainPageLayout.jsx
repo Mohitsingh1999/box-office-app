395a4a7ce7515a99f7186925837127607e7a7689
mohit <ms0135048@gmail.com>
2022-03-23 13:35:19 +0530

    addes search option

--- src/components/MainPageLayout.jsx
+++ src/components/MainPageLayout.jsx
@@ -5,7 +5,10 @@ import Title from './Title';
 function MainPageLayout({ children }) {
   return (
     <div>
-      <Title title="Box Ofiice" subtitle="Are you looking for movie or actor" />
+      <Title
+        title="Box Ofiice"
+        subtitle="Are you looking for movie or actor?"
+      />
       <Navs />
       {children}
     </div>
