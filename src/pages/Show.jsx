af368b17db823e113c74b9d1da9714c868ee31e0
mohit <ms0135048@gmail.com>
2022-03-26 16:00:28 +0530

    created two more hooks and used last query

--- src/pages/Show.jsx
+++ src/pages/Show.jsx
@@ -1,66 +1,25 @@
-import React, { useEffect, useReducer } from 'react';
+/* eslint-disable no-underscore-dangle */
+import React from 'react';
 import { useParams } from 'react-router-dom';
-import Cast from '../components/show/Cast';
+import ShowMainData from '../components/show/ShowMainData';
 import Details from '../components/show/Details';
 import Seasons from '../components/show/Seasons';
-import ShowMainData from '../components/show/ShowMainData';
-import { apiGet } from '../misc/config';
-import { InfoBlock, ShowPageWrapper } from './Show.styled';
-
-const reducer = (prevState, action) => {
-  switch (action.type) {
-    case 'FETCH_SUCCESS': {
-      return { isLoading: false, error: null, show: action.show };
-    }
-    case 'FETCH_FAILED': {
-      return { isLoading: false, erroe: action.error };
-    }
-    default:
-      return prevState;
-  }
-};
-
-const initialState = {
-  show: null,
-  isLoading: true,
-  error: null,
-};
+import Cast from '../components/show/Cast';
+import { ShowPageWrapper, InfoBlock } from './Show.styled';
+import { useShow } from '../misc/custom-hooks';
 
 const Show = () => {
   const { id } = useParams();
-  const [{ show, isLoading, error }, dispatch] = useReducer(
-    reducer,
-    initialState
-  );
-  // const [show, setShow] = useState(null);
-  // const [isLoading, setIsLoading] = useState(true);
-  // const [error, setError] = useState(null);
-  useEffect(() => {
-    let isMounted = true;
-    apiGet(`/shows/${id}?embed[]=seasons&embed[]=cast`)
-      .then(results => {
-        if (isMounted) {
-          dispatch({ type: 'FETCH_SUCCESS', show: results });
-          // setShow(results);
-          // setIsLoading(false);
-        }
-      })
-      .catch(err => {
-        dispatch({ type: 'FETCH_FAILED', error: err.message });
-        // setError(err.message);
-        // setIsLoading(false);
-      });
-    return () => {
-      isMounted = false;
-    };
-  }, [id]);
-  //console.log('Show ', show);
+  const { show, isLoading, error } = useShow(id);
+
   if (isLoading) {
     return <div>Data is being loaded</div>;
   }
+
   if (error) {
-    return <div>Error occured:{error}</div>;
+    return <div>Error occured: {error}</div>;
   }
+
   return (
     <ShowPageWrapper>
       <ShowMainData
